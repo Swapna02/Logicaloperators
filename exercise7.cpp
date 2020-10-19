af890dadf8cfb3f2852f41201a5e64ea11bf9aa5
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-10-19 23:49:26 +0500

    Added comments for Readability (Shahzain Ahmed)

--- exercise7.cpp
+++ exercise7.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
-
-
 using namespace std;
-
 main()
+int main 
 {
-    int firstElement, secondElement, divisionRemainder;
+    int firstElement, secondElement, divisionRemainder; // Declaring Variables with datatype of Integer (Shahzain Ahmed)
 
-    cout << "This is a simple program that calculates the remainder of division of two numbers." << endl;
+    cout << "This is a simple program that calculates the remainder of division of two numbers." << endl; // Displaying the string (Shahzain Ahmed)
     cout << "Input first number: ";
-    cin >> firstElement;
+    cin >> firstElement; // Taking input from the user (Shahzain Ahmed)
     cout << "Input second number: ";
-    cin >> secondElement;
+    cin >> secondElement; // Taking second input from the user (Shahzain Ahmed)
 
-    divisionRemainder = firstElement % secondElement;
+    divisionRemainder = firstElement % secondElement; // Formula for Division Remainder (Shahzain Ahmed)
     cout << "First number: " << firstElement << endl;
     cout << "Second number: " << secondElement << endl;
-    cout << "Remainder: " << divisionRemainder << endl;
-
+    cout << "Remainder: " << divisionRemainder << endl; // Displaying the final result (Shahzain Ahmed)
 }
-
-
