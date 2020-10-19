94057b8fbef4cbe5b0a414cefaec8f65698ed036
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-10-19 23:39:26 +0500

    updated by Shahzain Ahmed

--- exercise5.cpp
+++ exercise5.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
-
-
 using namespace std;
 
 main()
 {
-    double firstElement, secondElement, multiplicationResult;
-
+    double firstElement, secondElement, multiplicationResult; // declaring variables.
+cout << "This is a C++ Program." << endl; // Output display
     cout << "This is a simple multiplication program." << endl;
-    cout << "Input first number: ";
-    cin >> firstElement;
-    cout << "Input second number: ";
-    cin >> secondElement;
 
-    multiplicationResult = firstElement * secondElement;
+    cout << "Asking user for input: ";
+    cin >> firstElement; // Taking first input by Shahzain Ahmed
+    cout << "Asking user for second input";
+    cin >> secondElement; // Taking second input by Shahzain Ahmed
+
+    multiplicationResult = firstElement * secondElement; // Multiplication Formula written by Shahzain Ahmed
     cout << "First number: " << firstElement << endl;
     cout << "Second number: " << secondElement << endl;
-    cout << "Multiplication result: " << multiplicationResult << endl;
-
+    cout << "Multiplication result: " << multiplicationResult << endl; // Displaying the output after Multiplication by Shahzain Ahmed
+    
 }
 
 
