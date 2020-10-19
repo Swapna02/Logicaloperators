e46ac1d941452d5c6ed529105a300f89fbedf50f
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-10-19 23:54:28 +0500

    Added comments for readability (Shahzain Ahmed)

--- exercise1.cpp
+++ exercise1.cpp
@@ -1,18 +1,10 @@
 #include <iostream>
-
 using namespace std;
-
-main()
+int main()
 {
-    double lengthInches, lengthCentimeters;
-
-    cout << "Length in inches: ";
-    cin >> lengthInches;
-
-    lengthCentimeters = lengthInches * 2.54;
-
-    cout << lengthInches << " inches is equal to " << lengthCentimeters << " centimeters.";
-
+    double lengthInches, lengthCentimeters; // declaring variables with datatypes of Double (Shahzain Ahmed)
+    cout << "Length in inches: "; // displaying the string (Shahzain Ahmed)
+    cin >> lengthInches; // Taking input from the user (Shahzain Ahmed)
+    lengthCentimeters = lengthInches * 2.54; // Formula (Shahzain Ahmed)
+    cout << lengthInches << " inches is equal to " << lengthCentimeters << " centimeters."; // Displaying the final result (Shahzain Ahmed)
 }
-
-
