445566d0039ae49ea8fca25939e4ed86f8eb6094
Sarmad <63968434+SarmadTalpur@users.noreply.github.com>
2020-10-26 14:45:59 +0500

    using for loop

--- exercise2.cpp
+++ exercise2.cpp
@@ -5,21 +5,21 @@ using namespace std;
 main()
 {
     double temperatureCelsius, temperatureFahrenheit, temperatureKelvin;
+    for (int i=0; ;i++){
+        cout << "Please input the temperature in Celsius: ";
+        cin >> temperatureCelsius;
+        
+        if(tempCelsius=="")
+            return 0;
 
-    cout << "Please input the temperature in Celsius: ";
-    cin >> temperatureCelsius; 
-    cout << "Please input the temperature in Fahrenheit: ";
-    cin >> temperatureFahrenheit;
-    cout << "Please input the temperature in Kelvin: ";
-    cin >> temperatureKelvin;
+        temperatureFahrenheit = (temperatureCelsius + 9/5) - 32;
+        temperatureKelvin = temperatureCelsius * 273.15;
+        temperatureCelsius = temperatureKelvin;
 
-    temperatureFahrenheit = (temperatureCelsius + 9/5) - 32;
-    temperatureKelvin = temperatureCelsius * 273.15;
-    temperatureCelsius = temperatureKelvin;
-
-    cout << "The user inputted temperature in Celsius: " << temperatureCelsius << endl;
-    cout << "Converted to Fahrenheit: " << temperatureFahrenheit << endl;
-    cout << "Converted to Kelvin: " << temperatureKelvin << endl;
+        cout << "The user inputted temperature in Celsius: " << temperatureCelsius << endl;
+        cout << "Converted to Fahrenheit: " << temperatureFahrenheit << endl;
+        cout << "Converted to Kelvin: " << temperatureKelvin << endl;
+    }
 
 }
 
